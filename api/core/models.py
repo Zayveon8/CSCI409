2a666a0b9b535c7078f7d86ee1038c983d6d3a9c
Zayveon8 <zqhill@coastal.edu>
2024-02-14 11:30:34 -0500

    Made Database Modifications

--- api/core/models.py
+++ api/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Airport(models.Model):
     name = models.CharField(max_length=255)
     airport_code = models.CharField(max_length=3)
@@ -31,20 +30,19 @@ class Runway(models.Model):
     runway_designation = models.CharField(max_length=1, choices=RUNWAY_DESIGNATION_CHOICES)
     length = models.IntegerField()
     width = models.IntegerField()
-    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
+    airport = models.ForeignKey('Airport', on_delete=models.CASCADE, related_name='runways')
 
     def __str__(self):
         return f"{self.runway_number}{self.runway_designation}"
 
 class Flight(models.Model):
-    origin = models.ForeignKey('Airport', related_name='flight_origin', on_delete=models.PROTECT)
-    destination = models.ForeignKey('Airport', related_name='flight_destination', on_delete=models.PROTECT)
-    airline = models.ForeignKey('Airline', on_delete=models.PROTECT)
+    origin = models.ForeignKey('Airport', on_delete=models.PROTECT, related_name='flight_origin')
+    destination = models.ForeignKey('Airport', on_delete=models.PROTECT, related_name='flight_destination')
+    airline = models.ForeignKey('Airline', on_delete=models.PROTECT, related_name='flights')
     flight_number = models.IntegerField()
     departure = models.DateTimeField()
     arrival = models.DateTimeField()
     aircraft_type = models.CharField(max_length=10)
 
     def __str__(self):
-        return f"{self.airline.airline_code}{self.flight_number}"
-
+        return f"Flight {self.flight_number}"
